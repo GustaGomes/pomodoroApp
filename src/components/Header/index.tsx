2b417dd2ef5b6b24c1c9dab14fe3ee85872fc0ee
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-03-22 18:58:10 -0300

    add projeto git

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from "./styles";
+import Logo from "../../assets/img/Logo.svg";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={Logo} alt="Logo da aplicação" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24}/>
+        </NavLink>
+        <NavLink to="/History" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
---
+++ src/pages/History.tsx
@@ -0,0 +1,7 @@
+export function History(){
+    return (
+        <h1>Pagina de Historico </h1>
+    )
+}
+
+// export default History
