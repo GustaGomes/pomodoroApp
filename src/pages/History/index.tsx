2598df6df3dd7fb1e0c931e8668c5c1d804bb25f
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2024-04-03 12:40:27 -0300

    formate date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
@@ -8,7 +10,7 @@ export function History() {
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
-      
+
       <HistoryList>
         <table>
           <thead>
@@ -25,7 +27,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
